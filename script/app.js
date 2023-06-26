1a2242525da37f795533a47a555322817755ea16
juan jose alvarez <alvarez.juanjose@icloud.com>
2023-06-26 19:24:23 -0300

    carrito basico

--- script/app.js
+++ script/app.js
@@ -1,33 +1,34 @@
-// VARIABLES GENERALES
+// VARIABLES GENERALES ------------------------------------------------------------------------------------------------>
 
 let misCards = document.querySelector("#misCards");
-let botonesCompra = document.querySelector("#btnCompra");
-let btnFiltrarPrecio = document.querySelector("#btnFiltrarPrecio");
+let botones = document.getElementsByClassName("btn btn-dark btnCompra");
+const btnFiltrarPrecio = document.querySelector("#btnFiltrarPrecio");
 let inputMin = document.querySelector("#inputMin");
 let inputMax = document.querySelector("#inputMax");
-let btnFiltrarTitulo = document.querySelector("#btnFiltrarTitulo");
+const btnFiltrarTitulo = document.querySelector("#btnFiltrarTitulo");
 let inputTitulo = document.querySelector("#inputTitulo");
-let btnLimpiar = document.querySelector("#btnLimpiar");
-const carro = [];
+const btnLimpiar = document.querySelector("#btnLimpiar");
+let tablaCarrito = document.querySelector("#tablaCarrito");
+const carrito = [];
 
 
 console.table(libros);
 
 
-// FUNCIONES
+// FUNCIONES ---------------------------------------------------------------------------------------------------------->
 
 // Renderizar productos, visto en after02
-function renderizarProductos(listaProds){
+function renderizarProductos(listaLibros){
     misCards.innerHTML="";
     // primero en blanco para "resetear" cada vez que renderizo y no generar duplicados
-    for(const prod of listaProds){
+    for(const libro of listaLibros){
         misCards.innerHTML+=`  
             <div class="card text-center col-sm-12 col-md-5 col-lg-2 m-3 p-3" style="background-color: rgba(254,189,46,255)"> 
-                <img src=${prod.foto} class="card-img-top" alt="tapa">
+                <img src=${libro.foto} class="card-img-top" alt="tapa">
                 <div class="card-body">
-                    <h5 class="card-title">${prod.titulo}</h5>
-                    <p class="card-text">$ ${prod.precio}</p>
-                    <button id="btnCompra" class="btn btn-dark">Comprar</button>
+                    <h5 class="card-title">${libro.titulo}</h5>
+                    <p class="card-text">$ ${libro.precio}</p>
+                    <button id=${libro.id} class="btn btn-dark btnCompra">Comprar</button>
                 </div>
             </div>
         `;
@@ -36,11 +37,13 @@ function renderizarProductos(listaProds){
 
 renderizarProductos(libros);
 
+
 // Filtrar por precio, visto en after 03
 function filtrarPorPrecio(precioMin, precioMax){
     const filtradosPorPrecio = libros.filter((libro) => (libro.precio >= precioMin) && (libro.precio <= precioMax));
     return filtradosPorPrecio;
 }
+
 
 // Filtrar por titulo
 function filtrarPorTitulo(titulo){
@@ -48,15 +51,33 @@ function filtrarPorTitulo(titulo){
     return filtradosPorTitulo;
 }
 
+
 // Limpiar filtros
 function limpiar(){
     location.reload();
 }
 
+// Agregar al carrito
+function agregarCompra(libro){
+    carrito.push(libro);
+    console.table(carrito);
+    tablaCarrito.innerHTML += `
+        <tr>
+            <td>${libro.titulo}</td>
+            <td>${libro.autor}</td>
+            <td>${libro.universo}</td>
+            <td>$ ${libro.precio}</td>
+        </tr>
+    `;
+    
+    let totalPagar = carrito.reduce((ac, libro) => ac + libro.precio,0);
+    console.table(totalPagar);
+    document.querySelector("#totalPagar").innerText = `Total a pagar $:${totalPagar}`;
+    
+}
 
 
-
-// EVENTOS
+// EVENTOS --------------------------------------------------------------------------------------------------->
 
 // Filtrar por precio
 btnFiltrarPrecio.onclick = () => {
@@ -65,11 +86,6 @@ btnFiltrarPrecio.onclick = () => {
         console.table(listaFiltradosPrecio);
         renderizarProductos(listaFiltradosPrecio);
     }
-}
-
-// Limpiar filtros
-btnLimpiar.onclick = () => {
-    limpiar();
 }
 
 
@@ -83,7 +99,23 @@ btnFiltrarTitulo.onclick = () => {
 }
 
 
+// Limpiar filtros
+btnLimpiar.onclick = () => {
+    limpiar();
+}
 
-// --------------------------------------------------------------------------------------------------------------------// 
-// info
+
+// Agregar a carrito
+for(const boton of botones){
+    boton.addEventListener("click", () => {
+        const librosCarrito = libros.find((libro) => libro.id == boton.id);
+        console.table(librosCarrito);
+        agregarCompra(librosCarrito);
+    })
+}
+
+
+
+// INFO -----------------------------------------------------------------------------------------------------------> 
+
 // card - <div class="card col-sm-2 m-3 p-3" style="background-color: rgba(254,189,46,255)"> 
